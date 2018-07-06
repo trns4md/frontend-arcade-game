4c442c3aa5162390e071ce19f362124d0805699b
Becky White <trns4md@charter.net>
2018-07-05 23:31:14 -0400

    render player and enemy

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+class articles {
+    constructor() {
+        this.sprite = 'image/';
+        this.x = 2;
+        this.y = 5;
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+    }
+}
+class opponent extends articles {
+    constructor(x, y) {
+        super();
+        this.sprite += 'enemy-bug.png';
+        this.x = x;
+        this.y = y;
+    }
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -17,21 +27,28 @@ Enemy.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+/*Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+};*/
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
+class competitor extends articles {
+    constructor() {
+        super();
+        this.sprite += 'char-pink-girl.png';
+    }
+}
+const player = new competitor();
+player.render();
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+const allEnemies = new opponent();
 
-
+allEnemies = [...Array(3)];
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
